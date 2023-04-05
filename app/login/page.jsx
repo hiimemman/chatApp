32f89bb323aab846eec1d0fe9eda71beaf02b235
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-04-05 13:07:17 +0800

    use Link from nextLink

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,6 +2,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import {succesStatus, errorStatus, defaultStatus, successStatusLabel, errorStatusLabel, defaultStatusLabel} from '../design/button'
+import Link from "next/link"
 
 export default function Login(){
   const [emailStatus, setEmailStatus] = useState('blank')
@@ -17,7 +18,7 @@ export default function Login(){
     
     console.log(response)
     if(response.statusText === 'OK'){
-      setSubmitting(false)// turn of loading
+      setSubmitting(false)// turn off loading
     }
     if(response.data.requestStatus === 'Email does not exist'){
       setEmailStatus('error')
@@ -57,7 +58,7 @@ export default function Login(){
                 </div>
                 <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
             </div>
-            <a href="#" className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+            <Link href="#" className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</Link>
         </div>
         {submitting === true ? (<button disabled type="button" class="w-full text-white bg-blue-950 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -69,7 +70,7 @@ export default function Login(){
   ) : (<button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>)}
         
         <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+            Not registered? <Link href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</Link>
         </div>
     </form>
 </div>
